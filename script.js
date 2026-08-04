5fd8a6087b8a4dc083b6f79505a8e076c30c4ce6
ly202009 <leeyayoh@gmail.com>
2026-08-03 18:02:31 -0700

    Rotating Icons

---
+++ script.js
@@ -0,0 +1,29 @@
+const minScale = 0.5;
+const maxScale = 1;
+const minOpacity = 0.5;
+const maxOpacity = 1;
+var rotationSpeed = 0.002;
+
+
+function calculateCarousel(rotation, icon, total){
+  // Use carousel rotation and icon position to calculate position of the icon;
+  const localRotation = rotation + 360 * ((icon.style.getPropertyValue("--i"))/total);
+  // Use sin to determine the position between one side to the other within the box as well as cosine for depth
+  const depthRatio = (Math.cos(localRotation) + 1)/2;
+  const positionRatio = (Math.sin(localRotation) + 1)/2;
+
+  icon.style.left = `${positionRatio * 100}%`;
+  icon.style.transform = `scale(${depthRatio * (maxScale - minScale) + minScale})`;
+  icon.style.zIndex = Math.floor(depthRatio * 100);
+  icon.style.opacity = minOpacity + (maxOpacity - minOpacity) * depthRatio;
+}
+
+let rotation = 0; // initial rotation
+const icons = document.querySelectorAll(".about-icon");
+
+function animate(rotation){ // why was this so hard to figure out
+  icons.forEach((icon) => calculateCarousel(rotation, icon, 10));
+  requestAnimationFrame(() => animate(rotation+rotationSpeed));
+}
+
+animate(rotation);
