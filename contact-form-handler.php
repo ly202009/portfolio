ae160d959237a83585544d23bd218561eb5a2327
ly202009 <leeyayoh@gmail.com>
2026-08-12 21:10:20 -0700

    Add Basic PHP Functionality

---
+++ contact-form-handler.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+  name: <?php echo $_POST["name"];?><br>
+  email: <?php echo $_POST["email"];?><br>
+  subject: <?php echo $_POST["subject"];?><br>
+  message: <?php echo $_POST["message"];?><br>
+
+</body>
+</html>
